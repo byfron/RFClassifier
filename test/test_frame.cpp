640e0af0f30e3911c6071b98380c0af9c93f8d8c
byfron <josecrubio82@gmail.com>
2016-12-24 13:38:56 +0100

    Updates gitignore file

--- test/test_frame.cpp
+++ test/test_frame.cpp
@@ -1,7 +1,16 @@
 #include "gtest.h"
+#include <Frame.hpp>
 
 TEST(TestFrame, TestInit) {
 
-	EXPECT_TRUE(true);
+	std::string depth_path = std::string(DATA_FOLDER) + "/train/1/images/depthRender/Cam1/mayaProject.000001.png";
+	std::string gt_path = std::string(DATA_FOLDER) + "/train/1/images/groundtruth/Cam1/mayaProject.000001.png";
+
+	Frame f(depth_path, gt_path);
+
+	f.show();
 		
 }
+
+
+
